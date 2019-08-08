a44e1ff588f73dab7feb90afd6ecc8f47d614f8e
FrankiePP <sp3990@nyu.edu>
2019-08-08 19:22:39 -0400

    Visualized an ellipse over nigeria

--- assets/js/visualizer.js
+++ assets/js/visualizer.js
@@ -1,8 +1,29 @@
-function start(){
-    createCanvas(200, 200);
+
+let myMap;
+let canvas;
+const mappa = new Mappa('Leaflet');
+
+const options = {
+    lat: 0,
+    lng: 0,
+    zoom: 1.5,
+    style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+  }
+
+function setup(){
+   canvas =  createCanvas(640, 640);
+//    background(100);
+
+  // Create a tile map with lat 0, lng 0, zoom 4
+  myMap = mappa.tileMap(options); 
+  // Overlay the canvas over the tile map
+  myMap.overlay(canvas);
+
 }
 
 function draw(){
-    rectMode(CENTER);
-    rect(30,30,50,50);
+  // takes lat and long of the country nigeria and then converts it into a 2D vector.
+  const nigeria = myMap.latLngToPixel(11.396396,5.076543);
+  clear();
+  ellipse(nigeria.x, nigeria.y, 50, 50);
 }
